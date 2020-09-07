eb6a8b362a8640de6aa9b263426d256be834b635
Jim Blackler <jimblackler@google.com>
2020-09-07 08:30:58 +0100

    Broke out fixer into its own class.

--- src/main/java/net/jimblacker/jsongenerator/Generator.java
+++ src/main/java/net/jimblacker/jsongenerator/Generator.java
@@ -12,22 +12,10 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import net.jimblackler.jsonschemafriend.AnyOfValidationError;
-import net.jimblackler.jsonschemafriend.BelowMinItemsValidationError;
-import net.jimblackler.jsonschemafriend.ConstMismatchError;
 import net.jimblackler.jsonschemafriend.Ecma262Pattern;
 import net.jimblackler.jsonschemafriend.GenerationException;
-import net.jimblackler.jsonschemafriend.GreaterThanMaximumError;
-import net.jimblackler.jsonschemafriend.LessThanMinimumError;
-import net.jimblackler.jsonschemafriend.MissingPathException;
-import net.jimblackler.jsonschemafriend.MissingPropertyError;
-import net.jimblackler.jsonschemafriend.NotAMultipleError;
-import net.jimblackler.jsonschemafriend.NotValidationError;
-import net.jimblackler.jsonschemafriend.OneOfValidationError;
-import net.jimblackler.jsonschemafriend.PathUtils;
 import net.jimblackler.jsonschemafriend.Schema;
 import net.jimblackler.jsonschemafriend.SchemaStore;
-import net.jimblackler.jsonschemafriend.ValidationError;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -74,6 +62,10 @@ public class Generator {
     return number.doubleValue();
   }
 
+  public Schema getAnySchema() {
+    return anySchema;
+  }
+
   public Object generate(Schema schema, int maxTreeSize) throws JsonGeneratorException {
     if (schema.isFalse()) {
       throw new JsonGeneratorException(
@@ -83,126 +75,7 @@ public class Generator {
     random.setSeed(seed);
 
     Object object = generateUnvalidated(schema, maxTreeSize);
-    int attempts = 0;
-    while (true) {
-      Collection<ValidationError> errors = new ArrayList<>();
-      validate(schema, object, errors::add);
-      attempts++;
-      if (errors.isEmpty()) {
-        break;
-      }
-      if (attempts == 5) {
-        System.out.println("Object:");
-        if (object instanceof JSONObject) {
-          System.out.println(((JSONObject) object).toString(2));
-        } else if (object instanceof JSONArray) {
-          System.out.println(((JSONArray) object).toString(2));
-        } else {
-          System.out.println(object);
-        }
-        System.out.println();
-        for (ValidationError error : errors) {
-          System.out.println(error);
-        }
-        break;
-      }
-      List<ValidationError> errorList = new ArrayList<>(errors);
-      while (!errorList.isEmpty()) {
-        ValidationError error = errorList.remove(0);
-
-        if (error instanceof AnyOfValidationError) {
-          AnyOfValidationError error1 = (AnyOfValidationError) error;
-          List<List<ValidationError>> allErrors = error1.getAllErrors();
-          int i = random.nextInt(allErrors.size());
-          // Pick one at random and fix the object to match it.
-          errorList.addAll(allErrors.get(i));
-          continue;
-        }
-
-        if (error instanceof OneOfValidationError) {
-          OneOfValidationError error1 = (OneOfValidationError) error;
-          if (error1.getNumberPassed() == 0) {
-            List<List<ValidationError>> allErrors = error1.getAllErrors();
-            int i = random.nextInt(allErrors.size());
-            // Pick one at random and fix the object to match it.
-            errorList.addAll(allErrors.get(i));
-            continue;
-          }
-        }
-
-        try {
-          Object objectToFix = PathUtils.fetchFromPath(object, error.getUri().getRawFragment());
-          Object fixed = fix(objectToFix, error);
-          if (objectToFix == object) {
-            object = fixed;
-          } else if (objectToFix != fixed) {
-            PathUtils.modifyAtPath(object, error.getUri().getRawFragment(), fixed);
-          }
-        } catch (MissingPathException e) {
-          e.printStackTrace();
-        }
-      }
-    }
-
-    return object;
-  }
-
-  private Object fix(Object object, ValidationError error) {
-    if (error instanceof ConstMismatchError) {
-      ConstMismatchError error1 = (ConstMismatchError) error;
-      return error1.getConst();
-    }
-    if (error instanceof MissingPropertyError) {
-      MissingPropertyError error1 = (MissingPropertyError) error;
-      String property = error1.getProperty();
-      Schema schema = error.getSchema().getProperties().get(property);
-      JSONObject jsonObject = (JSONObject) object;
-      try {
-        Object value = generate(schema, 50);
-        jsonObject.put(property, value);
-      } catch (JsonGeneratorException e) {
-        e.printStackTrace();
-      }
-      return object;
-    }
-    if (error instanceof NotAMultipleError) {
-      NotAMultipleError error1 = (NotAMultipleError) error;
-      Number multiple = error1.getMultiple();
-      Number objectAsNumber = (Number) object;
-      double v = multiple.doubleValue();
-      int multiples = (int) (objectAsNumber.doubleValue() / v);
-      return multiples * v;
-    }
-
-    if (error instanceof LessThanMinimumError) {
-      LessThanMinimumError error1 = (LessThanMinimumError) error;
-      return error1.getMinimum();
-    }
-
-    if (error instanceof GreaterThanMaximumError) {
-      GreaterThanMaximumError error1 = (GreaterThanMaximumError) error;
-      return error1.getMaximum();
-    }
-
-    if (error instanceof NotValidationError) {
-      return object;
-    }
-
-    if (error instanceof BelowMinItemsValidationError) {
-      BelowMinItemsValidationError error1 = (BelowMinItemsValidationError) error;
-      Schema additionalItemsSchema = error.getSchema().getAdditionalItems();
-      JSONArray array = (JSONArray) object;
-
-      try {
-        if (additionalItemsSchema == null) {
-          array.put(generate(anySchema, 50));
-        } else {
-          array.put(generate(additionalItemsSchema, 50));
-        }
-      } catch (JsonGeneratorException e) {
-        e.printStackTrace();
-      }
-    }
+    object = Fixer.fixUp(schema, object, this, random);
 
     return object;
   }
