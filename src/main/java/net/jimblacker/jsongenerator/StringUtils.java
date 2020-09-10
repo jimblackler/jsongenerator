2cd20d037b3684a24369ece3d81183073d1c858e
Jim Blackler <jimblackler@google.com>
2020-09-10 14:47:18 +0100

    Minor format fix.

--- src/main/java/net/jimblacker/jsongenerator/StringUtils.java
+++ src/main/java/net/jimblacker/jsongenerator/StringUtils.java
@@ -13,7 +13,7 @@ public class StringUtils {
     StringBuilder stringBuilder = new StringBuilder();
     int length = random.nextInt(maxLength - minLength) + minLength;
     for (int idx = 0; idx != length; idx++) {
-      stringBuilder.append((char) (limitChars ? random.nextInt(96) + 32: random.nextInt()));
+      stringBuilder.append((char) (limitChars ? random.nextInt(96) + 32 : random.nextInt()));
     }
     // We don't aim to handle strings that won't survive URL encoding with standard methods.
     String urlEncoded = URLEncoder.encode(stringBuilder.toString());
